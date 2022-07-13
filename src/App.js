918dcb4229bd867334773871ffe8c80d6e9c91db
Nikita Kukshynsky <nkstatedev@gmail.com>
2022-07-13 18:06:27 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Nikita Kukshynsky</h1>
+        <h2>
+          <i>Software Engineer</i>
+        </h2>
+      </header>
+      <section>
+        <div className="Links">
+          <a target="_blank" rel="noreferrer" href="https://github.com/nikitaswe">
+            GitHub
+          </a>
+          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/swenikita">
+            LinkedIn
+          </a>
+          <a target="_blank" rel="noreferrer" href="https://leetcode.com/nikitaswe">
+            LeetCode
+          </a>
+        </div>
+      </section>
+      <section>
+        <div className="Bio">
+          <p>
+            A highly motivated self-taught programmer seeking to obtain a software engineering
+            position where I can grow and create software that has an impact and value for
+            clients/users. Like problem solving, passion to learn and expand knowledge in different
+            tech domains. Create projects with Java, Python and JavaScript. Apply data structures
+            and algorithms to write efficient code. Have experience with web development: databases,
+            front-end and back-end frameworks.
+          </p>
+        </div>
+      </section>
+      <header className="App-header">
+        <h3>More Coming Soon!</h3>
+      </header>
+    </div>
+  )
+}
+
+export default App
