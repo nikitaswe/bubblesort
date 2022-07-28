f60b391a68565e4e6ac6d936e27ec74036a0544e
Nikita Kukshynsky <nkstatedev@gmail.com>
2022-07-27 21:59:24 -0500

    updated links

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Nikita Kukshynsky</h1>
-        <h2>
-          <i>Software Engineer</i>
-        </h2>
+        <div className="Photo">
+          <img src="/photo.jpg" alt="Me" />
+        </div>
+
+        <div className="Titles">
+          <h1>Nikita Kukshynsky</h1>
+          <h2>
+            <i>Software Engineer</i>
+          </h2>
+        </div>
       </header>
       <section>
         <div className="Links">
-          <a target="_blank" rel="noreferrer" href="https://github.com/nikitaswe">
+          <a target="_blank" rel="noreferrer" href="https://github.com/nikitaxyz">
             GitHub
           </a>
-          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/swenikita">
+          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/nikitaxyz">
             LinkedIn
           </a>
           <a target="_blank" rel="noreferrer" href="https://leetcode.com/nikitaswe">
@@ -25,18 +31,23 @@ function App() {
       <section>
         <div className="Bio">
           <p>
-            A highly motivated self-taught programmer seeking to obtain a software engineering
-            position where I can grow and create software that has an impact and value for
-            clients/users. Like problem solving, passion to learn and expand knowledge in different
-            tech domains. Create projects with Java, Python and JavaScript. Apply data structures
-            and algorithms to write efficient code. Have experience with web development: databases,
-            front-end and back-end frameworks.
+            Thank you for visiting my website! You can check GitHub, LeetCode or LinkedIn profiles
+            via links above.
+          </p>
+          <p>
+            Willing to grow and create software that has an impact and value for clients/users. Like
+            problem solving, algorithmic design, learning new languages/frameworks/fields. Code in
+            Java, Python and JavaScript. Studied Electrical and Electronics Engineering. Apply data
+            structures and algorithms to create efficient code. Have experience in web development,
+            OOP, games. Ready for relocation.
           </p>
         </div>
       </section>
-      <header className="App-header">
-        <h3>More Coming Soon!</h3>
-      </header>
+      <footer className="App-footer">
+        <h3>
+          <a href="mailto:bubblesort.xyz@gmail.com">bubblesort.xyz@gmail.com</a>
+        </h3>
+      </footer>
     </div>
   )
 }
