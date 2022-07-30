bf84cee18b26f7e813ee647bcba2137229462bc8
Nikita Kukshynsky <nkstatedev@gmail.com>
2022-07-29 21:31:49 -0500

    new css and bio

--- src/App.js
+++ src/App.js
@@ -16,16 +16,24 @@ function App() {
         </div>
       </header>
       <section>
-        <div className="Links">
-          <a target="_blank" rel="noreferrer" href="https://github.com/nikitaxyz">
-            GitHub
-          </a>
-          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/nikitaxyz">
-            LinkedIn
-          </a>
-          <a target="_blank" rel="noreferrer" href="https://leetcode.com/nikitaswe">
-            LeetCode
-          </a>
+        <div className="links">
+          <ul>
+            <li>
+              <a target="_blank" rel="noreferrer" href="https://github.com/nikitaxyz">
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/nikitaxyz">
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a target="_blank" rel="noreferrer" href="https://leetcode.com/nikitaswe">
+                LeetCode
+              </a>
+            </li>
+          </ul>
         </div>
       </section>
       <section>
@@ -34,12 +42,41 @@ function App() {
             Thank you for visiting my website! You can check GitHub, LeetCode or LinkedIn profiles
             via links above.
           </p>
+        </div>
+        <div className="Bio">
           <p>
-            Willing to grow and create software that has an impact and value for clients/users. Like
-            problem solving, algorithmic design, learning new languages/frameworks/fields. Code in
-            Java, Python and JavaScript. Studied Electrical and Electronics Engineering. Apply data
-            structures and algorithms to create efficient code. Have experience in web development,
-            OOP, games. Ready for relocation.
+            I love coding and engineering! Bachelor's degree in Electrical and Electronics
+            Engineering (not the US), but now focused on becoming Full Stack Software Engineer.
+            Quickly learn new skills and programming languages, enjoy problem-solving, grind and
+            apply data structures and algorithms to make programs efficient. I create different
+            projects for learning purposes and curiosity: I'm always excited to learn and try
+            something new.
+          </p>
+          <p>As for languages, libraries, and frameworks I currently use:</p>
+          <ul>
+            <li>
+              <strong>Languages</strong>: Python, Java, JavaScript
+            </li>
+            <li>
+              <strong>Web Development</strong>: React, Node.JS, Express.JS, Django, Bootstrap
+            </li>
+            <li>
+              <strong>Databases & API</strong>: SQL, MongoDB, GraphQL, REST
+            </li>
+            <li>
+              <strong>Game development & desktop UI</strong>: PyGame, JavaFX
+            </li>
+            <li>
+              <strong>Other</strong>: C++, Git/GitHub, OOP, TDD, MVC, HTML, CSS, Jupyter Notebook,
+              Data Structures, Algorithms, basics of Machine Learning
+            </li>
+          </ul>
+        </div>
+        <div className="Bio">
+          <p>
+            I worked as a full-time Electrical Engineer, WordPress Developer, Stocker, and Grocery
+            Shopper. But I was always passionate about software engineering. Now it's time to show
+            my skills and apply them to delivering happiness!
           </p>
         </div>
       </section>
